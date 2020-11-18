0165022ee8310f38c81aaaf8b9d9cfd4295fb3fc
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2020-11-18 18:53:11 +0200

    adds test to convert fahrenheit to celsius adds logic to get test to pass

--- spec/TemperatureSpec.js
+++ spec/TemperatureSpec.js
@@ -1,9 +1,34 @@
-import TemperatureCalc from '../lib/TemperatureCalc';
+import TemperatureCalc from "../lib/TemperatureCalc";
 
-describe('TemperatureCalc', ()=> {
-  it('result is 0 when no temperature or type is provided', ()=>{
-    const temperature = new TemperatureCalc(0,null);
-    const actual = temperature.FahrenheitToCelsius()
-    expect(actual).toEqual(0)
-  })
+describe("TemperatureCalc", () => {
+  describe("Fahrenheit To Celsius", () => {
+    it("result is 0 when no temperature or type is provided", () => {
+      const temperature = new TemperatureCalc(0, null);
+      const actual = temperature.FahrenheitToCelsius();
+      expect(actual).toEqual(0);
+    });
+    it("converts 50 degrees Celsius to Fahrenheit", () => {
+      // F = (50 × 9 / 5) + 32
+
+      const temperature = new TemperatureCalc(50, "fahrenheit");
+      const actual = temperature.FahrenheitToCelsius();
+      expect(actual).toEqual(122);
+    });
+
+    it("converts 60 degrees Celsius to Fahrenheit", () => {
+      // F = (50 × 9 / 5) + 32
+
+      const temperature = new TemperatureCalc(60, "fahrenheit");
+      const actual = temperature.FahrenheitToCelsius();
+      expect(actual).toEqual(140);
+    });
+
+    it("converts 100 degrees Celsius to Fahrenheit", () => {
+      // F = (50 × 9 / 5) + 32
+
+      const temperature = new TemperatureCalc(100, "fahrenheit");
+      const actual = temperature.FahrenheitToCelsius();
+      expect(actual).toEqual(212);
+    });
+  });
 });
