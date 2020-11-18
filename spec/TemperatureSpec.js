241e465c3c5194d1fc21d14b63519a595bb4dec8
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2020-11-18 18:24:48 +0200

    setup babel to transpile ES6 code add first test case

--- spec/TemperatureSpec.js
+++ spec/TemperatureSpec.js
@@ -1,4 +1,9 @@
-describe('TemperatureCalc', ()=> {
-  var TemperatureCalc = require('../lib/TemperatureCalc');
+import TemperatureCalc from '../lib/TemperatureCalc';
 
+describe('TemperatureCalc', ()=> {
+  it('result is 0 when no temperature or type is provided', ()=>{
+    const temperature = new TemperatureCalc(0,null);
+    const actual = temperature.FahrenheitToCelsius()
+    expect(actual).toEqual(0)
+  })
 });
